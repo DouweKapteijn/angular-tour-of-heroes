a67ddc7c459c28bcb64bfc3181716821a4f97f28
DouweKapteijn <douwiaan@gmail.com>
2024-02-19 15:00:54 +0100

    message service test

--- src/app/message.service.spec.ts
+++ src/app/message.service.spec.ts
@@ -13,4 +13,17 @@ describe('MessageService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should add a message with add function', () => {
+    const message = 'Test Message';
+    service.add(message);
+    expect(service.messages).toContain(message);
+  });
+
+  it('should clear messages with clear function', () => {
+    service.add('Test Message 1');
+    service.add('Test Message 2');
+    service.clear();
+    expect(service.messages.length).toBe(0);
+  });
 });
